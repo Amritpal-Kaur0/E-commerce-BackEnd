c3298e752563bdc0617e0a72b58fb71e5cf1a862
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-06-06 21:01:27 +0530

    tag routes /del updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async(req, res) => {
   // be sure to include its associated Product data
   try {
     const TagsData = await Tag.findAll({
-      include: [{ model: Product }],
+      // include: [{ model: Product }],
     });
     res.status(200).json(TagsData);
   } catch (err) {
